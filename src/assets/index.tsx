c442a6f6f7f3d61127b6da0df833e476bb33d039
nateazevedo <nathanielazevedo@hotmail.com>
2023-03-23 17:03:00 -0700

    fix star import

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,7 +1,7 @@
 import close from "./close.svg";
 import logo from "./logo.svg";
 import menu from "./menu.svg";
-import star from "./star.svg";
+import star from "./Star.svg";
 import twitter from "./twitter.svg";
 import linkedin from "./linkedin.svg";
 
