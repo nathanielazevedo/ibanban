87d31342dfd07026325786343173134e7110d36e
nateazevedo <nathanielazevedo@hotmail.com>
2025-05-17 10:32:43 -0400

    change app.tsx for debugging

--- src/main.tsx
+++ src/main.tsx
@@ -1,45 +1,5 @@
-import "./index.css";
-import App from "./App";
-import React from "react";
-import authReducer from "./state";
-import { Provider } from "react-redux";
-import ReactDOM from "react-dom/client";
-import storage from "redux-persist/lib/storage";
-import { configureStore } from "@reduxjs/toolkit";
-import { PersistGate } from "redux-persist/integration/react";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
+const App = () => {
+  return <h1>Hello world</h1>;
+};
 
-const persistConfig = { key: "root", storage, version: 1 };
-const persistedReducer = persistReducer(persistConfig, authReducer);
-const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore(store)}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
-);
+export default App;
